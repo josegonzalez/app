ae7eec6847c76e69255d4cbfb528a70bf3135615
Jose Diaz-Gonzalez <josegonzalez@users.noreply.github.com>
2015-12-25 05:51:10 -0500

    Set `json` as a valid extensions
    `xml` is not considered a valid extension so as to discourage the proliferation of it.

--- config/routes.php
+++ config/routes.php
@@ -42,6 +42,8 @@ use Cake\Routing\Router;
 Router::defaultRouteClass('DashedRoute');
 
 Router::scope('/', function ($routes) {
+    $routes->extensions(['json']);
+
     /**
      * Here, we are connecting '/' (base path) to a controller called 'Pages',
      * its action called 'display', and we pass a param to select the view file
