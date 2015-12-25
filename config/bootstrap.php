a0a6d15ca555a274023827b7d8785d7416a4ec68
Jose Diaz-Gonzalez <josegonzalez@users.noreply.github.com>
2015-12-25 05:29:44 -0500

    Load all required plugins

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -181,6 +181,11 @@ Request::addDetector('tablet', function ($request) {
  */
 
 Plugin::load('Migrations');
+Plugin::load('BootstrapUI');
+Plugin::load('Crud');
+Plugin::load('CrudView');
+Plugin::load('Josegonzalez/Upload');
+Plugin::load('Search');
 
 // Only try to load DebugKit in development mode
 // Debug Kit should not be installed on a production system
