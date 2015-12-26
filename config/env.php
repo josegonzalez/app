ef50756b899f0ccf4e034769f8abbfb91a3de9e4
Jose Diaz-Gonzalez <josegonzalez@users.noreply.github.com>
2015-12-25 19:32:27 -0500

    Add support for loading environment variables for configuration from a .env file

---
+++ config/env.php
@@ -0,0 +1,42 @@
+<?php
+use josegonzalez\Dotenv\Loader;
+use Cake\Utility\Hash;
+
+$config = [];
+if (!env('APP_NAME')) {
+    $dotenv = new Loader([
+        __DIR__ . DS . '.env',
+        __DIR__ . DS . '.env.default',
+    ]);
+    $dotenv->setFilters([
+        'josegonzalez\Dotenv\Filter\LowercaseKeyFilter',
+        'josegonzalez\Dotenv\Filter\UppercaseFirstKeyFilter',
+        'josegonzalez\Dotenv\Filter\UnderscoreArrayFilter',
+        function ($data) {
+            $keys = [
+                'Debug' => 'debug',
+                'Emailtransport' => 'EmailTransport',
+                'Database' => 'Datasources.default',
+                'Test.database' => 'Datasources.test',
+                'Test' => null,
+                'Cache.duration' => null,
+                'Cache.cakemodel' => 'Cache._cake_model_',
+                'Cache.cakecore' => 'Cache._cake_core_',
+            ];
+            foreach ($keys as $key => $newKey) {
+                if ($newKey === null) {
+                    $data = Hash::remove($data, $key);
+                    continue;
+                }
+                $value = Hash::get($data, $key);
+                $data = Hash::remove($data, $key);
+                $data = Hash::insert($data, $newKey, $value);
+            }
+            return $data;
+        }
+    ]);
+    $dotenv->parse();
+    $dotenv->filter();
+    $config = $dotenv->toArray();
+}
+return $config;
