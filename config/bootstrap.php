b54343308120def3f3934ce68972777022ad0697
Jose Diaz-Gonzalez <josegonzalez@users.noreply.github.com>
2015-12-25 20:00:03 -0500

    Load the env configuration file

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -65,6 +65,7 @@ use Cake\Utility\Security;
 try {
     Configure::config('default', new PhpConfig());
     Configure::load('app', 'default', false);
+    Configure::load('env', 'default', true);
 } catch (\Exception $e) {
     die($e->getMessage() . "\n");
 }
